85757fb5ef9e02150cc375291c1406141f09aa78
masterUNG <phrombutr@gmail.com>
2015-09-03 11:27:46 +0700

    Create SQLite

--- app/src/main/java/appewtc/masterung/myrestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/myrestaurant/MainActivity.java
@@ -7,10 +7,23 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private UserTABLE objUserTABLE;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create & Connected Database
+        createAndConnectedDatabase();
+
+
+    }   // onCreate
+
+    private void createAndConnectedDatabase() {
+        objUserTABLE = new UserTABLE(this);
     }
 
     @Override
@@ -34,4 +47,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
